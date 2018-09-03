ffc39c45d462b22e8d3193ea228fd03e3cdf04ed
Meng Xiangzhuo <xzmong@icloud.com>
2018-09-04 06:12:57 +0800

    v0.1基本的文字目录和图片浏览.ok.

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 from flask import Flask
 from flask import render_template
+import os
 
 app = Flask(__name__)
 
+resource_dir = 'static/mm131.com'
+
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def index():
+    girl_list = os.listdir(resource_dir)
+    return render_template('index.html', girl_list=girl_list)
 
+
+@app.route('/girl/<title>')
+def girl_page(title):
+    girl_path = os.path.join(resource_dir, title)
+    pic_list = os.listdir(girl_path)
+    pic_path_list = map(lambda x: os.path.join(girl_path, x), pic_list)
+    return render_template('girl_page.html', girl_path=girl_path,
+                           title=title, pic_path_list=pic_path_list)
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
 
 if __name__ == '__main__':
-    app.run(debug=1)
+    app.run()
---
+++ test.py
@@ -0,0 +1,4 @@
+import os
+resource_dir = 'static/mm131.com'
+for item in os.listdir(resource_dir):
+    print(item)
